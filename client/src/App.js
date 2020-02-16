49b32f91d7c5265729251813c168011129e919cb
Wolfaholic <melissa.mackie@live.com>
2020-02-15 17:28:31 -0800

    saving progress.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieCard from './Movies/MovieCard';
+import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +15,11 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+      <Route path="/" exact component={MovieList} />
+      <Route path="/Movies/:id" component={Movie} /> />
+        />
+      </div>
     </div>
   );
 };
